8c7e2b1ae91141d8d82b199274624a2ffb3bb7d3
Alex Norris <akwnorris@gmail.com>
2023-11-06 21:58:46 -0600

    create hero carousel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import HeroSlider from './HeroSlider'; // Import your HeroSlider component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header role="banner">
+        {/* ... Your header content ... */}
       </header>
+      <main role="main">
+        <HeroSlider /> {/* Render the HeroSlider component here */}
+      </main>
+      <footer role="contentinfo">
+        {/* ... Your footer content ... */}
+      </footer>
     </div>
   );
 }
--- src/HeroSlider.js
+++ src/HeroSlider.js
@@ -1,25 +1,36 @@
-const slides = document.querySelectorAll('.carousel-slide');
-const prevButton = document.getElementById('prev-button');
-const nextButton = document.getElementById('next-button');
-let currentIndex = 0;
+import React, { useState } from 'react';
 
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.style.display = i === index ? 'block' : 'none';
-  });
+const images = [
+  "/images/baby clothes.jpg",
+  "/images/toys.jpg",
+  "/images/christmas.jpg",
+];
+
+function HeroSlider() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentIndex((currentIndex - 1 + images.length) % images.length);
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((currentIndex + 1) % images.length);
+  };
+
+  return (
+    <div className="carousel-container">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className={`carousel-slide ${index === currentIndex ? 'active' : ''}`}
+        >
+          <img src={image} alt={`Image ${index + 1}`} />
+        </div>
+      ))}
+      <button id="prev-button" onClick={prevSlide}>Previous</button>
+      <button id="next-button" onClick={nextSlide}>Next</button>
+    </div>
+  );
 }
 
-function prevSlide() {
-  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-  showSlide(currentIndex);
-}
-
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % slides.length;
-  showSlide(currentIndex);
-}
-
-prevButton.addEventListener('click', prevSlide);
-nextButton.addEventListener('click', nextSlide);
-
-showSlide(currentIndex);
+export default HeroSlider;
