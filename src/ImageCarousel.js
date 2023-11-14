d02cbff1266a74eb9ca76d66be432cebd351af57
Alex Norris <akwnorris@gmail.com>
2023-11-14 09:56:27 -0600

    Add buttons to carousel items

--- src/ImageCarousel.js
+++ src/ImageCarousel.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import './index.css'; // Import a separate CSS file for styling
+import './index.css';
 
 const ImageCarousel = () => {
   return (
     <Carousel className="main-carousel" autoPlay interval={3000} infiniteLoop showStatus={false}>
-      <div>
+      <div className="carousel-item">
         <img src="/baby clothes.jpg" alt="Baby Clothes Image" className="carousel-image" />
+        <button className="carousel-button">Button for Clothes</button>
       </div>
-      <div>
+      <div className="carousel-item">
         <img src="/toys.jpg" alt="Baby Toys Image" className="carousel-image" />
+        <button className="carousel-button">Button for Toys</button>
       </div>
-      <div>
+      <div className="carousel-item">
         <img src="/babyxmas.jpg" alt="Baby's First Christmas Image" className="carousel-image" />
+        <button className="carousel-button">Button for Christmas</button>
       </div>
     </Carousel>
   );
